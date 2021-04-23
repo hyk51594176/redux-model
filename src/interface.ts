fd49c192f28151083bd90d118b35852af1d4eaf6
hanyukai <51594176@qq.com>
2021-04-23 18:33:37 +0800

    feat: ts

--- src/interface.ts
+++ src/interface.ts
@@ -9,52 +9,54 @@ import { ReducersMapObject, Dispatch, Middleware, Action } from 'redux';
 
 declare module 'redux' {
   export interface Dispatch {
-    (type: string, params?: any): Promise<any>
+    (type: string, params?: any): Promise<any>;
   }
 }
 export interface ModelAction extends Action {
-  payload: any
+  payload: any;
 }
 export interface Reducers<S> {
-  [key: string]: (state: S, data: any) => S
+  [key: string]: (state: S, data: any) => S;
 }
 export interface ActionStoreAPi<S> {
-  dispatch: Dispatch
-  getState(): S
-  commit(type: string, payload: any): void
+  dispatch: Dispatch;
+  getState(): S;
+  commit(type: string, payload: any): void;
 }
 export interface Actions<S> {
-  [key: string]: (store: ActionStoreAPi<S>, data: any) => Promise<any>
+  [key: string]: (store: ActionStoreAPi<S>, data: any) => Promise<any>;
 }
 export interface Model<S = any, K extends keyof S = any> {
-  namespace: K
-  state: S[K]
-  reducers?: Reducers<S[K]>
-  actions?: Actions<S>
+  namespace: K;
+  state: S[K];
+  reducers?: Reducers<S[K]>;
+  actions?: Actions<S>;
 }
 
 export interface RootModel<S = any> {
-  separator: string
-  state: S
-  reducers: ReducersMapObject<S, ModelAction>
+  separator: string;
+  state: S;
+  reducers: ReducersMapObject<S, ModelAction>;
   actions: {
-    [K in keyof S]: Actions<S>
-  }
+    [K in keyof S]: Actions<S>;
+  };
 }
 
 export interface Options<S> {
-  separator: string
-  reducers: ReducersMapObject<S, ModelAction>
-  middlewares: Array<Middleware<any, S, any>>
-  loadingModel?: boolean
+  separator: string;
+  reducers: ReducersMapObject<S, ModelAction>;
+  middlewares: Array<Middleware<any, S, any>>;
+  loadingModel?: boolean;
 }
 
 export interface Loading<S> {
   loading: {
     [K in keyof S]: {
-      [T in keyof Model<S, K>['actions']]: number
-    }
-  } & { globalLoading: 0 }
+      [T in keyof Model<S, K>['actions']]: number;
+    };
+  } & { globalLoading: 0 };
 }
 
 export type State<S> = S & Loading<S>;
+
+export const defineModel = <S = any, K extends keyof S = any>(data: Model<S, K>) => data;
